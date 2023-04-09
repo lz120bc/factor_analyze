c129440e895db29fe3a897cd3e4b0b3c75aab869
Kim <202210150045@uibe.edu.cn>
2023-04-09 16:17:45 +0800

    update

--- reg.py
+++ reg.py
@@ -1,14 +1,16 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import statsmodels.api as sm
 import datetime
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import statsmodels.api as sm
 
 var = []
 var2 = []
+data_save = '/Users/lvfreud/Documents/python/factor_data/'
+output = '/Users/lvfreud/Desktop/'
 
 
-def roll_reg(roll_data):
+def roll_reg(roll_data):  # rolling_regress
     dm = roll_data['ym'].max()
     x = roll_data[var]
     x = sm.add_constant(x)
@@ -30,9 +32,9 @@ def main(con_str, roll_days, stock_num, main_var):
     var.extend(['inc', 'ebi'])
     var2 = main_var.copy()
     var2.extend(['incs', 'ebis'])
-    data = pd.read_csv(r'/Users/lvfreud/Documents/python/factor_data/su.csv')
+    data = pd.read_csv(data_save + 'su.csv')
+    concept = pd.read_excel(data_save + 'concept.xlsx')
     data['ym'] = pd.to_datetime(data['ym'])
-    concept = pd.read_excel('/Users/lvfreud/Documents/python/factor_data/concept.xlsx')
     concept = concept.drop(index=concept.index.values[-4:])
     concept = concept.rename(
         columns={'所属概念板块\n[交易日期] 最新收盘日': 'cc', '所属热门概念\n[交易日期] 最新收盘日': 'nc'})
@@ -53,7 +55,6 @@ def main(con_str, roll_days, stock_num, main_var):
     for d in date:
         rdata = data.drop(data[(data['ym'] > d + delta) | (data['ym'] < d)].index)
         y_pz, pz, r2z = roll_reg(rdata)
-
         if len(y_sum) == 0:
             y_sum = y_pz
             p_sum = pz
@@ -65,13 +66,11 @@ def main(con_str, roll_days, stock_num, main_var):
     st = st['ym']
     st = st.rename('st')
     y_sum = y_sum.merge(st, how='left', on='Stkcd')
-    y_sum['stm'] = y_sum['ym']-y_sum['st']
+    y_sum['stm'] = y_sum['ym'] - y_sum['st']
     y_sum = y_sum.drop(y_sum[y_sum[var].duplicated()].index)
-    df = y_sum
-    su = data  # 原始收益率序列
     de = datetime.timedelta(days=300)
-    df = df[df['stm'] > de]  # 筛选成立300天的企业
-    select_arr = df.sort_values(by=['r_p'], ascending=[False]).reset_index(drop=True)
+    y_sum = y_sum[y_sum['stm'] > de]  # 筛选成立300天的企业
+    select_arr = y_sum.sort_values(by=['r_p'], ascending=[False]).reset_index(drop=True)
     select_arr = select_arr.groupby(['ym']).head(stock_num)
     select_arr = select_arr.sort_values(by=['ym'], ascending=[True]).reset_index(drop=True)  # 选股序列
     da = select_arr['ym'].drop_duplicates()  # 获取时间序列
@@ -80,19 +79,16 @@ def main(con_str, roll_days, stock_num, main_var):
         u = select_arr[select_arr['ym'] == i]['r_p']
         idd = select_arr[select_arr['ym'] == i]['Stkcd']
         idd = idd.reset_index(drop=True)
-        cor = su[su['Stkcd'] == idd[0]][['ym', 'r_su']]
+        cor = data[data['Stkcd'] == idd[0]][['ym', 'r_su']]
         for j in idd:  # 获取收益率矩阵
             if j == idd[0]:
                 continue
-            m = su[su['Stkcd'] == j][['ym', 'r_su']]
-            m = m.rename(columns={'r_su': 'r_su_'+str(j)})
-            cor = cor.merge(m, on=['ym'])
+            m = data[data['Stkcd'] == j][['ym', 'r_su']]
+            m = m.rename(columns={'r_su': 'r_su_' + str(j)})
+            cor = cor.merge(m, on=['ym'], how='outer')
         cor = cor.fillna(0)
-        co = cor[(cor['ym'] <= i) & (cor['ym'] > i-delta)].iloc[:, 1:].corr()
-        try:
-            w = np.dot(np.linalg.inv(co), u)  # 求各股权重
-        except:
-            w = np.ones(stock_num)
+        co = cor[(cor['ym'] <= i) & (cor['ym'] > i - delta)].iloc[:, 1:].corr()
+        w = np.dot(np.linalg.inv(co), u)  # 根据收益率相关性以及效用函数设定，求各股权重
         w = w * (w > 0)
         if w.sum() != 0:
             w = w / w.sum()
@@ -103,10 +99,11 @@ def main(con_str, roll_days, stock_num, main_var):
     results = select_arr[['Stkcd', 'w']].iloc[-stock_num:, :]
     results['板块'] = con_str
     results['r'] = syl.iloc[-1].values[0]
-    return results
+    # 绘图略
     # syl.plot(figsize=(18, 8), ylim=[0, 6])
     # plt.savefig('si.png')
     # plt.show()
+    return results
 
 
 if __name__ == '__main__':
@@ -119,4 +116,4 @@ if __name__ == '__main__':
           main('新能源', 300, 4, ['to_sd', 'ret_m', 'r_su', 'r_v', 'lnd_m']),
           main('饮料', 200, 4, ['to_sd', 'ret_m', 'r_v', 'lnd_m'])]
     re = pd.concat(re)
-    re.to_excel('/Users/lvfreud/Desktop/select.xlsx', index=False)
+    re.to_excel(output + 'select.xlsx', index=False)
